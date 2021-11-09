4bc82ae7c640b1d9ec3d399750eca6e711d4a886
Gamzeen <demirgamzenn@gmail.com>
2021-11-09 23:01:36 +0300

    --comm

---
+++ Enums/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        enum Level
+        {
+            Low,
+            Medium,
+            High
+        }
+        static void Main(string[] args)
+        {
+            Level level = Level.Low;
+            switch (level)
+            {
+                case Level.Low:
+                    Console.WriteLine("Level Low"); 
+                    break;
+                case Level.Medium:
+                    Console.WriteLine("Level Medium"); 
+                    break;
+                case Level.High:
+                    Console.WriteLine("Level High"); 
+                    break;
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
